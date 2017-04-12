bb9e3d9cbfaaddb15541330fd4cd623b53c9d525
zengwh <1421245334@qq.com>
2017-04-12 11:31:25 +0800

    the forth commit

---
+++ src/com/mlq/love/entity/Employee.java
@@ -0,0 +1,46 @@
+package com.mlq.love.entity;
+
+import java.util.Date;
+
+public class Employee {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String age;
+	
+	private Date employedDate;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public Date getEmployedDate() {
+		return employedDate;
+	}
+
+	public void setEmployedDate(Date employedDate) {
+		this.employedDate = employedDate;
+	}
+}
--- src/com/mlq/love/test/TestLog4j2.java
+++ src/com/mlq/love/test/TestLog4j2.java
@@ -1,4 +1,4 @@
-package com.mlq.love;
+package com.mlq.love.test;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
